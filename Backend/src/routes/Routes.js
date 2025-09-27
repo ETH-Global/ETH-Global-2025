0ff8e781c17b54e8a9ddb6ecaf5fb3e0d17e1839
karma <captainav0608@gmail.com>
2025-09-27 17:27:16 +0530

    [feature] add verify route

--- Backend/src/routes/Routes.js
+++ Backend/src/routes/Routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/Controllers");
+const kyc = require("../controllers/verify.js");
 require("dotenv").config();
 
 
@@ -10,6 +11,10 @@ router.get("/", (req,res) => {
 
 router.post("/poll", (req, res) => {
     controllers.CleansObj(req,res);
+});
+
+router.post("/verify", (req, res) => {
+    kyc.UserVerification(req, res);
 });
 
 module.exports = router;
